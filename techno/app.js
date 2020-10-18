4cedbfc83f3c564dfde4d65bec4c6a54e26dbe78
Luis Gustavo <gustavoluisrh@gmail.com>
2020-10-18 20:29:08 -0300

    Criando uma rota com js

--- techno/app.js
+++ techno/app.js
@@ -4,13 +4,15 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false
   },
   filters: {
     numeroPreco(valor){
-      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
     },
     maiuscula(valor) {
-      return valor.toUpperCase()
+      return valor.toUpperCase();
     }
    },
    computed:{
@@ -55,10 +57,11 @@ const vm = new Vue({
     },
     adicionarItem() {
       this.produto.estoque--;
-
       const { id, nome, preco } = this.produto;
-
       this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho`);
+
+     
     },
     removerItem(index) {
       this.carrinho.splice(index, 1);
@@ -67,9 +70,27 @@ const vm = new Vue({
       if(window.localStorage.carrinho) {
         this.carrinho = JSON.parse(window.localStorage.carrinho);
       }
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+       setTimeout(()=> {
+        this.alertaAtivo = false
+      }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if(hash) {
+        this.fetchProduto(hash.replace("#", ""));
+      }
     }
   },
   watch:{
+    produto(){
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || ""
+      history.pushState(null, null, `#${hash}`)
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
     }
@@ -77,5 +98,6 @@ const vm = new Vue({
   created() {
     this.fetchProdutos();
     this.checarLocalStorage();
+    this.router();
   }
 })
